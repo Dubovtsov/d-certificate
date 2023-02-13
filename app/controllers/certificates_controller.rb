14dfad621e1cffc616e3a0f2d59ccf62f9ffc677
adm Дубовцов Иван Александрович <adm-DubovtsovIA@m14.dzm>
2023-02-13 11:15:09 +0300

    employee positions

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,27 +1,25 @@
 class CertificatesController < ApplicationController
-  before_action :set_certificate, only: %i[ show edit update destroy ]
-  before_action :set_employee
+  before_action :set_certificate, only: %i[show edit update destroy]
+  before_action :set_employee, except: [:index]
 
   def index
     @certificates = Certificate.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @certificate = @employee.certificates.build
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @certificate = @employee.certificates.build(certificate_params)
 
     respond_to do |format|
       if @certificate.save
-        format.html { redirect_to employee_url(@employee), notice: "Certificate was successfully created." }
+        format.html { redirect_to employee_url(@employee), notice: 'Certificate was successfully created.' }
         format.json { render :show, status: :created, location: @certificate }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -33,7 +31,7 @@ class CertificatesController < ApplicationController
   def update
     respond_to do |format|
       if @certificate.update(certificate_params)
-        format.html { redirect_to employee_url(@employee), notice: "Certificate was successfully updated." }
+        format.html { redirect_to employee_url(@employee), notice: 'Certificate was successfully updated.' }
         format.json { render :show, status: :ok, location: @certificate }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,22 +44,23 @@ class CertificatesController < ApplicationController
     @certificate.destroy
 
     respond_to do |format|
-      format.html { redirect_to certificates_url, notice: "Certificate was successfully destroyed." }
+      format.html { redirect_to certificates_url, notice: 'Certificate was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-    def set_certificate
-      @certificate = Certificate.find(params[:id])
-    end
+  def set_certificate
+    @certificate = Certificate.find(params[:id])
+  end
 
-    def set_employee
-      @employee = Employee.find(params[:employee_id])
-    end
+  def set_employee
+    @employee = Employee.find(params[:employee_id])
+  end
 
-    def certificate_params
-      params.require(:certificate).permit(:legal_entity, :request_number, :request_link, :end_date, :status, :e_service, :employee_id)
-    end
+  def certificate_params
+    params.require(:certificate).permit(:legal_entity, :request_number, :request_link, :end_date, :status,
+                                        :e_service, :employee_id)
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,32 +1,70 @@
 class Employee < ApplicationRecord
+
   require 'csv'
+
   include GenerateCsv
 
+
+
   has_many :employee_positions, dependent: :destroy
+
   has_many :positions, through: :employee_positions, dependent: :destroy
+
   accepts_nested_attributes_for :employee_positions
+
   has_one :personal_datum, dependent: :destroy
+
   has_many :certificates, dependent: :destroy
+
   accepts_nested_attributes_for :personal_datum
+
   accepts_nested_attributes_for :employee_positions
+
+
+
+  validates :last_name, presence: true
+
+  validates :first_name, presence: true
+
+
 
   before_save :normalize_name
 
+
+
   def name_with_initial
+
     "#{last_name.capitalize} #{first_name.capitalize} #{middle_name.capitalize}"
+
   end
 
+
+
   def self.import(file)
+
     CSV.foreach(file.path, headers: true) do |row|
+
       Employee.create! row.to_hash unless Employee.exists?(last_name: row['last_name'], first_name: row['first_name'])
+
     end
+
   end
+
+
 
   private
 
+
+
   def normalize_name
+
     self.first_name = first_name.upcase
+
     self.last_name = last_name.upcase
+
     self.middle_name = middle_name.upcase
+
   end
+
 end
+
