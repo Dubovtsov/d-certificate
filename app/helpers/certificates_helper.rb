09d3238bc407f56523f0e784c1f8779bda01074c
Dubovtsov <dubwin@yandex.ru>
2023-02-14 16:55:48 +0300

    button path cert

--- app/helpers/certificates_helper.rb
+++ app/helpers/certificates_helper.rb
@@ -1,2 +1,14 @@
 module CertificatesHelper
+  STATUS = [
+    'черновик',
+    'на проверке',
+    'действующий',
+    'отклонен',
+    'архив',
+    'аннулирован'
+  ]
+
+  def status_for_select
+    STATUS.each { |status| status }
+  end
 end
