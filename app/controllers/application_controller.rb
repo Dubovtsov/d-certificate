09847f147577c3cbd1cefb4dc62df34c5797cb85
Dubovtsov <dubwin@yandex.ru>
2023-02-18 17:01:32 +0300

    fix layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-
-  layout :layout_by_resource
-
-  protected
-
-  def layout_by_resource
-    if devise_controller?
-      "devise"
-    else
-      "application"
-    end
-  end
 end
